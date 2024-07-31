a0d5b30e0e8a4c90159cc855f4249222d3a32d89
David G <himynamesdave@users.noreply.github.com>
2024-07-31 14:52:45 +0100

    Update test_8_0_sigma_to_cve.py

--- tests/test_8_0_sigma_to_cve.py
+++ tests/test_8_0_sigma_to_cve.py
@@ -46,7 +46,25 @@ class TestArangoDB(unittest.TestCase):
         ]
         self.assertEqual(result_count, expected_ids, f"Expected {expected_ids}, but found {result_count}.")
 
-# test 2 Expecting 4 results (see test-data-research.md for info as to why).
+    def test_02_check_generated_relationships(self):
+        query = """
+        RETURN LENGTH(
+          FOR doc IN sigma_rules_edge_collection
+            FILTER doc._is_latest == true
+            AND doc.relationship_type == "detects"
+            AND doc._arango_cti_processor_note == "sigma-cve"
+            AND doc.object_marking_refs == [
+                "marking-definition--94868c89-83c2-464b-929b-a1a8aa3c8487",
+                "marking-definition--2e51a631-99d8-52a5-95a6-8314d3f4fbf3"
+            ]
+            RETURN [doc]
+        )
+        """
+        result_count = self.run_query(query)
+        self.assertEqual(result_count, [15546], f"Expected 15546 documents, but found {result_count}.")
+
+
+# test 3 Expecting 4 results (see test-data-research.md for info as to why).
 # `indicator--74904ec1-cff3-5737-a1d4-408c789dc8b1`
 # links to `cve.2022.26134` (`vulnerability--b4fd2041-12ff-5a64-9c00-51ba39b29fe4`) and `cve.2021.26084` (`vulnerability--ff040ea3-f2d9-5d38-80ae-065a2db41e64`)
 ###`2e51a631-99d8-52a5-95a6-8314d3f4fbf3` `detects+sigma_rules_vertex_collection/indicator--74904ec1-cff3-5737-a1d4-408c789dc8b1+nvd_cve_vertex_collection/vulnerability--b4fd2041-12ff-5a64-9c00-51ba39b29fe4` = `relationship--71b515ec-a8bc-5b92-a732-8f2f7cfea05f`
@@ -59,7 +77,7 @@ class TestArangoDB(unittest.TestCase):
 ###`2e51a631-99d8-52a5-95a6-8314d3f4fbf3` `detects+sigma_rules_vertex_collection/indicator--57ddd7ed-0a84-57ad-8fc0-dd892346ec32+nvd_cve_vertex_collection/vulnerability--570304ae-02cf-542b-ab7a-77e7ada2f48e` = `relationship--d08f9a4d-1eed-562e-ae7c-0aecbfacf5ea`
 
 
-    def test_02_check_generated_relationships(self):
+    def test_03_check_generated_relationships(self):
         query = """
           FOR doc IN sigma_rules_edge_collection
               FILTER doc.relationship_type == "detects"
