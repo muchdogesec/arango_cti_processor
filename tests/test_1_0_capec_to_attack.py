907ebd5187b231d92fc078315fa10039f850c350
David G <himynamesdave@users.noreply.github.com>
2024-08-02 13:39:15 +0100

    adding new test data

--- tests/test_1_0_capec_to_attack.py
+++ tests/test_1_0_capec_to_attack.py
@@ -3,13 +3,23 @@ import subprocess
 import unittest
 from arango import ArangoClient
 from dotenv import load_dotenv
+from stix2arango.stix2arango import Stix2Arango
+
+from .upload import make_uploads
 
 # Load environment variables
 load_dotenv()
 
-ARANGODB_USERNAME = os.getenv("ARANGODB_USERNAME")
-ARANGODB_PASSWORD = os.getenv("ARANGODB_PASSWORD")
-ARANGODB_HOST_URL = os.getenv("ARANGODB_HOST_URL")
+ARANGODB_USERNAME = os.getenv("ARANGODB_USERNAME", "root")
+ARANGODB_PASSWORD = os.getenv("ARANGODB_PASSWORD", "")
+ARANGODB_HOST_URL = os.getenv("ARANGODB_HOST_URL", "http://127.0.0.1:8529")
+
+print(f"ARANGODB_USERNAME: {ARANGODB_USERNAME}")
+print(f"ARANGODB_PASSWORD: {ARANGODB_PASSWORD}")
+print(f"ARANGODB_HOST_URL: {ARANGODB_HOST_URL}")
+
+if not ARANGODB_USERNAME or not ARANGODB_HOST_URL:
+    raise ValueError("ARANGODB_USERNAME and ARANGODB_HOST_URL must be set. Please check your .env file.")
 
 client = ArangoClient(hosts=f"{ARANGODB_HOST_URL}")
 
@@ -17,6 +27,15 @@ class TestArangoDB(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
+        subprocess.run([
+            "python3", "tests/delete_all_databases.py"
+        ], check=True)
+        make_uploads([
+            ("mitre_attack_enterprise", "tests/files/enterprise-attack-14_1.json"),
+            ("mitre_attack_ics", "tests/files/ics-attack-14_1.json"),
+            ("mitre_attack_mobile", "tests/files/mobile-attack-14_1.json"),
+            ("mitre_capec", "tests/files/cwe-bundle-v4_13.json"),
+        ], database="arango_cti_processor_standard_tests", delete_db=True)
         # Run the arango_cti_processor.py script
         subprocess.run([
             "python3", "arango_cti_processor.py",
@@ -48,7 +67,7 @@ class TestArangoDB(unittest.TestCase):
 
         self.assertEqual(result_count, expected_ids, f"Expected {expected_ids}, but found {result_count}.")
 
-# test 2 checks all objects generated correctly
+    # test 2 checks all objects generated correctly
 
     def test_02_arango_cti_processor_note(self):
         query = """
@@ -63,7 +82,7 @@ class TestArangoDB(unittest.TestCase):
 
         self.assertEqual(result_count, [346], f"Expected 346 documents, but found {result_count}.")
 
-# checks the corret number of objects are generated, and that they are assigned the correct properties by the script
+    # checks the corret number of objects are generated, and that they are assigned the correct properties by the script
 
     def test_03_correct_object_properties(self):
         query = """
@@ -85,7 +104,7 @@ class TestArangoDB(unittest.TestCase):
 
         self.assertEqual(result_count, [346], f"Expected 338 documents, but found {result_count}.")
 
-# test 4 checks [CAPEC-695](https://github.com/mitre/cti/blob/master/capec/2.1/attack-pattern/attack-pattern--e3dd79e7-307b-42dd-9e22-d0345c0ec001.json) -> T1195.001
+    # test 4 checks [CAPEC-695](https://github.com/mitre/cti/blob/master/capec/2.1/attack-pattern/attack-pattern--e3dd79e7-307b-42dd-9e22-d0345c0ec001.json) -> T1195.001
 
     def test_04_correct_relationship_capec695(self):
         query = """
@@ -104,7 +123,7 @@ class TestArangoDB(unittest.TestCase):
 
         self.assertEqual(result_count, expected_ids, f"Expected {expected_ids}, but found {result_count}.")
 
-# test 5 is extension of test 4 to check id generation: `2e51a631-99d8-52a5-95a6-8314d3f4fbf3` value: `technique+mitre_capec_vertex_collection/attack-pattern--e3dd79e7-307b-42dd-9e22-d0345c0ec001+mitre_attack_enterprise_vertex_collection/attack-pattern--191cc6af-1bb2-4344-ab5f-28e496638720` = 34d58beb-5d4c-5899-b3aa-2a0d5995e82d
+    # test 5 is extension of test 4 to check id generation: `2e51a631-99d8-52a5-95a6-8314d3f4fbf3` value: `technique+mitre_capec_vertex_collection/attack-pattern--e3dd79e7-307b-42dd-9e22-d0345c0ec001+mitre_attack_enterprise_vertex_collection/attack-pattern--191cc6
 
     def test_05_relationship_id_generation695(self):
         query = """
@@ -123,7 +142,7 @@ class TestArangoDB(unittest.TestCase):
 
         self.assertEqual(result_count, expected_ids, f"Expected {expected_ids}, but found {result_count}.")
 
-# test 6 checks [CAPEC-233](https://github.com/mitre/cti/blob/master/capec/2.1/attack-pattern/attack-pattern--c05fff04-b965-4a11-9c18-379dac31969f.json) -> T1548
+    # test 6 checks [CAPEC-233](https://github.com/mitre/cti/blob/master/capec/2.1/attack-pattern/attack-pattern--c05fff04-b965-4a11-9c18-379dac31969f.json) -> T1548
 
     def test_06_correct_relationship_capec233(self):
         query = """
@@ -142,7 +161,7 @@ class TestArangoDB(unittest.TestCase):
 
         self.assertEqual(result_count, expected_ids, f"Expected {expected_ids}, but found {result_count}.")
 
-# test 7 is extension of test 6 to check id generation: `2e51a631-99d8-52a5-95a6-8314d3f4fbf3` `technique+mitre_capec_vertex_collection/attack-pattern--c05fff04-b965-4a11-9c18-379dac31969f+mitre_attack_enterprise_vertex_collection/attack-pattern--67720091-eee3-4d2d-ae16-8264567f6f5b` = bc0d764e-f1ba-55ff-b871-7735ec140789
+    # test 7 is extension of test 6 to check id generation: `2e51a631-99d8-52a5-95a6-8314d3f4fbf3` `technique+mitre_capec_vertex_collection/attack-pattern--c05fff04-b965-4a11-9c18-379dac31969f+mitre_attack_enterprise_vertex_collection/attack-pattern--67720091-eee3-4d2d-ae16-8264567f6f5b` = bc0d764e-f1ba-55ff-b871-7735ec140789
 
     def test_07_relationship_id_generation233(self):
         query = """
@@ -161,7 +180,7 @@ class TestArangoDB(unittest.TestCase):
 
         self.assertEqual(result_count, expected_ids, f"Expected {expected_ids}, but found {result_count}.")
 
-# test 8 checks [CAPEC-13](https://github.com/mitre/cti/blob/master/capec/2.1/attack-pattern/attack-pattern--f190e1b3-e8d6-4aef-817c-b3e7782e2aed.json -> T1562.003, T1574.006, T1574.007
+    # test 8 checks [CAPEC-13](https://github.com/mitre/cti/blob/master/capec/2.1/attack-pattern/attack-pattern--f190e1b3-e8d6-4aef-817c-b3e7782e2aed.json -> T1562.003, T1574.006, T1574.007
 
     def test_08_correct_relationship_capec13(self):
         query = """
@@ -182,10 +201,10 @@ class TestArangoDB(unittest.TestCase):
 
         self.assertEqual(result_count, expected_ids, f"Expected {expected_ids}, but found {result_count}.")
 
-# test 9 is extension of test 8 to check id generation:
-# object id `2e51a631-99d8-52a5-95a6-8314d3f4fbf3` `technique+mitre_capec_vertex_collection/attack-pattern--f190e1b3-e8d6-4aef-817c-b3e7782e2aed+mitre_attack_enterprise_vertex_collection/attack-pattern--8f504411-cb96-4dac-a537-8d2bb7679c59` = c1100173-c259-52a1-926d-48919b067224
-# object id `2e51a631-99d8-52a5-95a6-8314d3f4fbf3` `technique+mitre_capec_vertex_collection/attack-pattern--f190e1b3-e8d6-4aef-817c-b3e7782e2aed+mitre_attack_enterprise_vertex_collection/attack-pattern--633a100c-b2c9-41bf-9be5-905c1b16c825` = d66bcc7d-19c0-5c2c-a7f7-44b13d6af09c
-# object id `2e51a631-99d8-52a5-95a6-8314d3f4fbf3` `technique+mitre_capec_vertex_collection/attack-pattern--f190e1b3-e8d6-4aef-817c-b3e7782e2aed+mitre_attack_enterprise_vertex_collection/attack-pattern--0c2d00da-7742-49e7-9928-4514e5075d32` = 23bd53cf-0f53-53e3-8ba1-92b4077bd460
+    # test 9 is extension of test 8 to check id generation:
+    # object id `2e51a631-99d8-52a5-95a6-8314d3f4fbf3` `technique+mitre_capec_vertex_collection/attack-pattern--f190e1b3-e8d6-4aef-817c-b3e7782e2aed+mitre_attack_enterprise_vertex_collection/attack-pattern--8f504411-cb96-4dac-a537-8d2bb7679c59` = c1100173-c259-52a1-926d-48919b067224
+    # object id `2e51a631-99d8-52a5-95a6-8314d3f4fbf3` `technique+mitre_capec_vertex_collection/attack-pattern--f190e1b3-e8d6-4aef-817c-b3e7782e2aed+mitre_attack_enterprise_vertex_collection/attack-pattern--633a100c-b2c9-41bf-9be5-905c1b16c825` = d66bcc7d-19c0-5c2c-a7f7-44b13d6af09c
+    # object id `2e51a631-99d8-52a5-95a6-8314d3f4fbf3` `technique+mitre_capec_vertex_collection/attack-pattern--f190e1b3-e8d6-4aef-817c-b3e7782e2aed+mitre_attack_enterprise_vertex_collection/attack-pattern--0c2d00da-7742-49e7-9928-4514e5075d32` = 23bd53cf-0f53-53e3-8ba1-92b4077bd460
 
     def test_09_relationship_id_generation13(self):
         query = """
@@ -206,7 +225,7 @@ class TestArangoDB(unittest.TestCase):
 
         self.assertEqual(result_count, expected_ids, f"Expected {expected_ids}, but found {result_count}.")
 
-# no updates to the objects should have happened yet
+    # no updates to the objects should have happened yet
 
     def test_10_check_no_updates(self):
         query = """
